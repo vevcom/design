5d5b32b235b11c2b27d2f926876d895d56e50433
Trym Sneltvedt <trysne@gmail.com>
2019-06-26 22:42:40 +0200

    Fix style errors

--- demo/App.vue
+++ demo/App.vue
@@ -1,13 +1,25 @@
 <template>
-    <Wrapper>
-      <Title>Vevcom Design</Title>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus ex quis consectetur feugiat. Aliquam sed elit quam. Nulla enim nisl, varius sed tortor vitae, posuere eleifend lacus. Quisque efficitur tincidunt velit, sed viverra nunc laoreet eu. Duis tincidunt, enim ac semper fringilla, leo neque efficitur est, id commodo diam dolor vel massa. Nulla at feugiat nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam porta ullamcorper felis, in ornare nunc dictum sit amet. Sed elementum nunc nibh, eu lacinia massa porttitor in. Etiam vel condimentum libero. Praesent eget hendrerit dolor. Phasellus hendrerit turpis arcu, ut finibus odio consectetur ut. Aliquam vitae libero ut dolor sollicitudin sollicitudin. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu velit id mauris fringilla egestas. Pellentesque imperdiet pulvinar velit congue pretium. 
-      </Paragraph>
-      <Buttons>
-        <Button primary>Very good</Button>
-        <Button>Okay</Button>
-        <Button danger>Not good</Button>
-      </Buttons>
-    </Wrapper>
+  <Wrapper>
+    <Title>Vevcom Design</Title>
+    <Paragraph>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin finibus ex
+      quis consectetur feugiat. Aliquam sed elit quam. Nulla enim nisl, varius
+      sed tortor vitae, posuere eleifend lacus. Quisque efficitur tincidunt
+      velit, sed viverra nunc laoreet eu. Duis tincidunt, enim ac semper
+      fringilla, leo neque efficitur est, id commodo diam dolor vel massa. Nulla
+      at feugiat nulla. Vestibulum ante ipsum primis in faucibus orci luctus et
+      ultrices posuere cubilia Curae; Etiam porta ullamcorper felis, in ornare
+      nunc dictum sit amet. Sed elementum nunc nibh, eu lacinia massa porttitor
+      in. Etiam vel condimentum libero. Praesent eget hendrerit dolor. Phasellus
+      hendrerit turpis arcu, ut finibus odio consectetur ut. Aliquam vitae
+      libero ut dolor sollicitudin sollicitudin. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit. Mauris eu velit id mauris fringilla egestas.
+      Pellentesque imperdiet pulvinar velit congue pretium.
+    </Paragraph>
+    <Buttons>
+      <Button primary>Very good</Button>
+      <Button>Okay</Button>
+      <Button danger>Not good</Button>
+    </Buttons>
+  </Wrapper>
 </template>
