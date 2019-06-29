4636e511826210b6b6e18df29256aea3c151c1cc
Trym Sneltvedt <trysne@gmail.com>
2019-06-29 11:29:15 +0200

    Add horizontal padding to wrapper

--- src/components/atoms/Wrapper.vue
+++ src/components/atoms/Wrapper.vue
@@ -5,10 +5,12 @@
 </template>
 
 <style lang="scss">
+@import "../../scss/variables";
+
 .ow-contentwrapper {
   max-width: 640px;
   min-width: 320px;
   margin: 0 auto;
-  padding-top: 2em;
+  padding: 2em $size-base 0;
 }
 </style>
